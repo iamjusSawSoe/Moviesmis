eba16751b1c0256a16bd0b4cb37ab0c7dc83e5f9
Saw Soe <92997329+iamjusSawSoe@users.noreply.github.com>
2024-09-09 00:15:44 +0630

    fix: Update Watch Now

--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -1,7 +1,129 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+import SwiperCore, { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "animate.css";
+import Axios from "axios";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+import { FaPlay } from "react-icons/fa";
+import { useQuery } from "react-query";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import apiConfig from "../api/apiConfig";
+import { toggleModalDetail } from "../store/Modal";
+import Button from "./Button";
 type Props = { name: string; category: string; type: string };
 
 const FilmList = (props: Props) => {
-  return <div>FilmList</div>;
+  SwiperCore.use([Autoplay]);
+  const isModalDetail = useSelector((state) => state.modal.isModalDetail);
+  const dispatch = useDispatch();
+  const [itemId, setItemid] = useState("");
+  const [detailModal, setDetailModal] = useState(false);
+  const swiperRef = useRef(null);
+
+  const { data, isError } = useQuery(
+    ["film", props.category, props.type],
+    async () => {
+      return await Axios.get(
+        `${apiConfig.baseUrl}${props.category}/${props.type}/${apiConfig.apiKey}&page=1`
+      ).then((res) => res.data.results);
+    },
+    {
+      staleTime: 30000,
+      cacheTime: 60000,
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  if (isError) {
+    return <h1> Sorry, there was an error </h1>;
+  }
+
+  useEffect(() => {
+    document.body.style.overflow = isModalDetail ? "hidden" : "auto";
+  }, [isModalDetail]);
+
+  const openModalDetail = (id) => {
+    setDetailModal(!detailModal);
+    swiperRef.current.swiper.autoplay.stop();
+    setItemid(id);
+  };
+
+  return (
+    <section className=" mx-6 sm:mx-10 my-10 ">
+      <div className="flex justify-between items-center ">
+        <h1 className="dark:text-white text-[#13191d] text-[20px] sm:text-[40px] leading-3 sm:text-2xl font-bold ">
+          {props.name}
+        </h1>
+        <Link to={`/${props.category === "movie" ? "movies" : "series"}`}>
+          <Button
+            styles=" rounded-full py-0 sm:py-1 px-6 sm:px-8 font-[400] hover:font-[800] ease-in-out duration-500  "
+            text="More..."
+          />
+        </Link>
+      </div>
+      <AnimationOnScroll
+        duration="2.5"
+        animateIn="animate__slideInRight"
+        animateOnce="true"
+      >
+        <div className=" mt-8 ">
+          <Swiper
+            ref={swiperRef}
+            modules={[Autoplay]}
+            grabCursor={true}
+            spaceBetween={10}
+            slidesPerView="auto"
+            loopedSlides={20}
+            loop={true}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: true,
+              pauseOnMouseEnter: true,
+            }}
+          >
+            {data?.map((item, index) => (
+              <SwiperSlide
+                key={index}
+                className="group xl:w-[11.5%] md:w-[15.1%] lmd:w-[20%] sm:w-[24.2%] ss:w-[28%] w-[45%] h-[320px]"
+              >
+                <div
+                  className="h-[305px] rounded-3xl hover:scale-[1.05] hover:border-[3px]  hover:ease-in-out hover:duration-150 hover:border-secondary "
+                  onClick={() => {
+                    openModalDetail(item.id);
+                    dispatch(toggleModalDetail());
+                  }}
+                >
+                  <FaPlay className=" cursor-pointer absolute my-32 mx-20 text-4xl invisible group-hover:visible group-hover:ease-in-out group-hover:duration-500 z-50 text-transparent group-hover:text-secondary" />
+                  <img
+                    src={apiConfig.w500Image(item.poster_path)}
+                    className=" h-[300px] rounded-3xl object-fill cursor-pointer group-hover:brightness-[.45]"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </AnimationOnScroll>
+
+      {/* {detailModal && (
+        <DetailModal
+          openModalDetail={() => openModalDetail()}
+          itemId={itemId}
+          itemCategory={props.category}
+          itemType={props.type}
+        />
+      )} */}
+    </section>
+  );
 };
 
 export default FilmList;
--- src/components/WatchNow.tsx
+++ src/components/WatchNow.tsx
@@ -1,51 +1,47 @@
 "use client";
 
-import { useImageMovie } from "@/hooks/useImageMovie";
-import { useImageTvSeries } from "@/hooks/useImageTvSeries";
 import { useTrending } from "@/hooks/useTrending";
 import { useEffect, useRef, useState } from "react";
-import { useDispatch } from "react-redux";
 import "swiper/css";
 import "swiper/css/autoplay";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Button from "./Button";
 
+const preloadImage = (src: string) => {
+  const img = new Image();
+  img.src = src;
+};
+
 const WatchNow = () => {
   const swiperRef = useRef(null);
-  const [itemId, setItemid] = useState("");
-  const dispatch = useDispatch();
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-
-  const [movieId, setMovieId] = useState<number | undefined>(undefined);
-  const [seriesId, setSeriesId] = useState<number | undefined>(undefined);
+  const [isImagesLoaded, setIsImagesLoaded] = useState(false);
+  const [allSlidesData, setAllSlidesData] = useState<any[]>([]);
 
   const trendingQuery = useTrending();
 
-  const movieQuery = useImageMovie(movieId);
-  const seriesQuery = useImageTvSeries(seriesId);
-
   useEffect(() => {
-    if (
-      trendingQuery.data &&
-      trendingQuery.data.length > 0 &&
-      trendingQuery.data.media_type === "movie"
-    ) {
-      setMovieId(trendingQuery.data[0].id);
+    if (trendingQuery.data) {
+      // Map through trending data and extract the image URLs
+      const slidesData = trendingQuery.data.map((item: any) => {
+        let image = "";
+        if (item.media_type === "movie") {
+          image = `${process.env.NEXT_PUBLIC_ORIGINAL_IMAGE_URL}${item.backdrop_path}`;
+        } else if (item.media_type === "tv") {
+          image = `${process.env.NEXT_PUBLIC_ORIGINAL_IMAGE_URL}${item.backdrop_path}`;
+        }
+
+        return { ...item, image };
+      });
+
+      setAllSlidesData(slidesData);
+      setIsImagesLoaded(true);
     }
   }, [trendingQuery.data]);
 
-  useEffect(() => {
-    if (
-      trendingQuery.data &&
-      trendingQuery.data.length > 0 &&
-      trendingQuery.data.media_type === "tv"
-    ) {
-      setSeriesId(trendingQuery.data[0].id);
-    }
-  }, [trendingQuery.data]);
-
-  if (trendingQuery.isPending || !trendingQuery.data)
+  if (trendingQuery.isPending || !isImagesLoaded) {
     return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -60,7 +56,6 @@ const WatchNow = () => {
           speed={1000}
           onSlideChange={(swiper) => {
             setActiveSlideIndex(swiper.realIndex);
-            setMovieId(trendingQuery.data[swiper.realIndex]?.id);
           }}
           autoplay={{
             delay: 6000,
@@ -68,33 +63,33 @@ const WatchNow = () => {
             pauseOnMouseEnter: true,
           }}
         >
-          {trendingQuery.data?.map((movie, index) => (
-            <SwiperSlide key={movie.id}>
+          {allSlidesData?.map((item, index) => (
+            <SwiperSlide key={item.id}>
               <div
-                className={`w-full max-h-[1100px] h-[1100px] lmd:max-h-[100vh] lmd:h-[100vh] bg-no-repeat bg-cover `}
+                className={`w-full h-[100vh] bg-no-repeat bg-cover`}
                 style={{
-                  backgroundImage: `url('${process.env.NEXT_PUBLIC_ORIGINAL_IMAGE_URL}/${movieQuery.data?.posters?.[0]?.file_path}')`,
+                  backgroundImage: `url('${item.image}')`,
                 }}
               >
                 <div className="w-full h-full bg-dimBlack flex flex-col-reverse lg:flex-row justify-center items-center gap-10">
                   <div className="lg:max-w-[800px] mb-28 lmd:mb-40 lg:mb-0">
                     <h1
-                      className={` text-white font-bold text-[30px] lg:text-[45px] leading-[40px] lmd:leading-[30px] mb-6 lmd:mb-10  ${
+                      className={` text-white font-bold text-[30px] lg:text-[45px] leading-[40px] lmd:leading-[30px] mb-6 lmd:mb-10 ${
                         activeSlideIndex === index
                           ? "animate__animated animate__fadeInDown animate__slow"
                           : ""
                       }`}
                     >
-                      {movie.name}
+                      {item.name || item.title}
                     </h1>
                     <p
                       className={`${
                         activeSlideIndex === index
-                          ? "animate__animated animate__fadeInDown animate__slow	"
+                          ? "animate__animated animate__fadeInDown animate__slow"
                           : ""
                       } flex text-white font-semibold px-4 lmd:px-0 text-[16px]  lg:text-[18px] leading-[40px] lmd:w-[800px] lg:w-[650px] text-center`}
                     >
-                      {movie.overview}
+                      {item.overview}
                     </p>
                     <div
                       className={`mt-8  flex ss:block gap-4 ${
